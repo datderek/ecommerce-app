9335a0673e991b50b8dbaa73592e19a09ab7e47f
Derek Tran <tran.derek.d@gmail.com>
2024-05-23 09:51:05 -0700

    Add Posts to Home page

---
+++ src/components/Post.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import styles from './Post.module.css';
+
+function Post({ title, subtitle, imageUrl, imageDescription, children }) {
+  return (
+    <div className={styles['post-wrapper']}>
+      <img className={styles['post-image']} src={imageUrl} alt={imageDescription}/>
+      <div className={styles['post-details']}>
+        <h4>{subtitle}</h4>
+        <h3>{title}</h3>
+        <p>{children}</p>
+      </div>
+      <Link className={styles['read-more-button']} to='/post'>Read more</Link>
+    </div>
+  )
+}
+
+export default Post;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,10 @@
 import styles from './Home.module.css';
 import headerBackgroundImage from '../assets/coffee-farm.jpg';
 import InfiniteCarousel from '../components/InfiniteCarousel';
+import Post from '../components/Post';
+import coffeeOriginImage from '../assets/coffee-origin.avif';
+import coffeeRoastImage from '../assets/coffee-roast.avif';
+import coffeeBrewImage from '../assets/coffee-brew.avif';
 
 const images = [
   'https://iili.io/H8Y5Sgj.webp',
@@ -19,7 +23,7 @@ function Home() {
 
   return (
     <main>
-      <section className={styles.header} style={{ backgroundImage: `url(${headerBackgroundImage})`}}>
+      <section className={styles.header} style={{ backgroundImage: `url(${headerBackgroundImage})` }}>
         <div className={styles['header-content-wrapper']}>
           <div className={styles['header-content']}>
             <h2>Because the best days start with a</h2>
@@ -30,10 +34,36 @@ function Home() {
       </section>
       <section className={styles['product-preview']}>
         <h2>Check out some of our products!</h2>
-        <InfiniteCarousel images={images}/>
+        <InfiniteCarousel images={images} />
       </section>
       <section className={styles.blog}>
-
+        <h2>Posts from our community</h2>
+        <div className={styles['posts-wrapper']}>
+          <Post
+            title={'Where Our Beans Come From'}
+            subtitle={'Sustainability'}
+            imageUrl={coffeeOriginImage}
+            imageDescription={''}
+          >
+            Learn how choosing ethically sourced beans not only supports the environment and local farmers but also enhances the quality and flavor of your daily brew.
+          </Post>
+          <Post
+            title={"Light to Dark Roast - What's the Difference"}
+            subtitle={'Educational'}
+            imageUrl={coffeeRoastImage}
+            imageDescription={''}
+          >
+            From the bright, acidic notes of a light roast to the bold, robust flavors of a dark roast, understanding these distinctions will help you find your perfect cup.
+          </Post>
+          <Post
+            title={'How to Make the Perfect Cup of Coffee'}
+            subtitle={'Tutorial'}
+            imageUrl={coffeeBrewImage}
+            imageDescription={''}
+          >
+            Whether you're a novice or a seasoned barista, these tips and techniques will help you achieve a rich, flavorful brew every time, ensuring your mornings start on the right note.
+          </Post>
+        </div>
       </section>
       <footer>
 
